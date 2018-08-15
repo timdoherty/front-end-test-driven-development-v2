89b899bc94f42fadbcd46291cba0cb5a9f4f7a35
Tim Doherty <tim.doherty@procore.com>
2018-08-15 07:34:51 -0700

    Completed step 1

---
+++ src/search/Search.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Input } from '@procore/core-react';
+
+import Search from './Search';
+
+describe('<Search/>', () => {
+  beforeEach(() => {
+    // setup each test here (or use before() for all)
+  });
+
+  afterEach(() => {
+    // tear down each test here (or use after() for all)
+  });
+
+  it('has somewhere to enter search text', () => {
+    const wrapper = shallow(
+      <Search />
+    );
+    expect(wrapper.find(Input).exists()).toBe.true;
+  });
+
+  describe('when the user presses enter', () => {
+    it('reacts when there is a search term', () => {
+      const onSearchChangedMock= jest.fn();
+      const wrapper = shallow(
+        <Search onSearchChanged={onSearchChangedMock} />
+      );
+
+      wrapper.find(Input).simulate('keyup', { key: 'Enter', target: { value: 'foo' } });
+      expect(onSearchChangedMock).toBeCalledWith('foo');
+    });
+
+    it('does not react when there is no search term', () => {
+      const onSearchChangedMock= jest.fn();
+      const wrapper = shallow(
+        <Search onSearchChanged={onSearchChangedMock} />
+      );
+
+      wrapper.find(Input).simulate('keyup', { key: 'Enter', target: { value: '' } });
+      expect(onSearchChangedMock).not.toBeCalled();
+    });
+  });
+});
