536ac2605cf530367cf458f54eb56de6fcb3d0c6
Tim Doherty <tim.doherty@procore.com>
2018-08-17 12:55:08 -0700

    checkpoint

--- src/components/thumbnail/Thumbnail.js
+++ src/components/thumbnail/Thumbnail.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Card } from '@procore/core-react';
 
 export default function Thumbnail(props) {
-  const { imageUrl } = props;
+  const { imageUrl, height, width, duration } = props;
   return (
-    <img src={imageUrl} />
+    <Card level="10" style={{ height: `${height}px`, width: `${width}px` }}>
+      <img src={imageUrl} height={`${height}`} width={`${width}`} />
+      <div>{duration}</div>
+    </Card>
   );
 }
---
+++ src/components/thumbnail/Thumbnail.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Card } from '@procore/core-react';
+
+import Thumbnail from './Thumbnail';
+
+describe('<Thumbnail/>', () => {
+  beforeEach(() => {
+    // setup each test here (or use before() for all)
+  });
+
+  afterEach(() => {
+    // tear down each test here (or use after() for all)
+  });
+
+  it('displays an image', () => {
+    const url = "foobarbaz";
+    const wrapper = shallow(
+      <Thumbnail imageUrl={url} />
+    );
+
+    expect(wrapper.find('img').prop('src')).toBe(url);
+  });
+
+  it('has dimensions', () => {
+    const style = { height: '100px', width: '300px' };
+    const wrapper = shallow(
+      <Thumbnail height={100} width={300} />
+    );
+
+    expect(wrapper.find(Card).prop('style')).toEqual(style);
+    expect(wrapper.find('img').prop('height')).toEqual('100');
+    expect(wrapper.find('img').prop('width')).toEqual('300');
+  });
+
+  it('shows duration', () => {
+    const wrapper = shallow(
+      <Thumbnail duration="foobarbaz" /> 
+    );
+
+    expect(wrapper.html()).toMatch(/foobarbaz/);
+  });
+});
