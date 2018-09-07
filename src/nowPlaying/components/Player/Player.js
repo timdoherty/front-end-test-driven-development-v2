9492e062d2a32fcd8997bc8b04df076bdcf725e4
Tim Doherty <tim.doherty@procore.com>
2018-09-07 13:26:48 -0700

    checkpoint

--- src/nowPlaying/components/Player/Player.js
+++ src/nowPlaying/components/Player/Player.js
@@ -30,12 +30,16 @@ export default function Player(props) {
           width="720" height="405"
         />
       </div>
-      <span>{title}</span>
+      <div className="title-container">
+        <span className="title">{title}</span>
+        <span>{`${viewCount} views`}</span>
+        <div>
+          <span>{likeCount}</span>
+          <span>{dislikeCount}</span>
+        </div>
+      </div>
       <span>{description}</span>
       <span>{channelTitle}</span>
-      <span>{`${viewCount} views`}</span>
-      <span>{likeCount}</span>
-      <span>{dislikeCount}</span>
       <span>{`${commentCount} comments`}</span>
     </section>
   );
