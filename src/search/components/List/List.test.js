228daf53d37a8e7ca4d0807dbbbb300790e829d0
Tim Doherty <tim.doherty@procore.com>
2018-09-10 13:19:01 -0700

    Update List.test.js

--- src/search/components/List/List.test.js
+++ src/search/components/List/List.test.js
@@ -2,31 +2,27 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import List from './List';
-import Preview from '../../../components/Preview';
+import SearchResult from '../SearchResult/SearchResult';
 import searchResultStubs from '../../stubs/searchResultsStub';
 import searchMetadataStubs from '../../stubs/searchMetadataStub';
 import searchSelector from '../../selector';
 
 describe('<List/>', () => {
-  let searchResults;
-  beforeEach(() => {
-    const state = {
-      search: {
-        searchResults: searchResultStubs,
-        searchMetadata: searchMetadataStubs
-      }
-    };
-    searchResults = searchSelector(state).searchResults;
-  });
+  let searchResults = searchSelector({
+    search: {
+      searchResults: searchResultStubs,
+      searchMetadata: searchMetadataStubs
+    }
+  }).searchResults;
 
   it('displays a search result for each item', () => {
     const wrapper = shallow(
       <List searchResults={searchResults} />
     );
-    expect(wrapper.find(Preview).length).toBe(searchResults.length);
+    expect(wrapper.find(SearchResult).length).toBe(searchResults.length);
   });
 
-  it('responds with the right video id when an item is clicked', () => {
+  it('responds with the right video when an item is clicked', () => {
     const onListItemClickedMock = jest.fn();
     const wrapper = shallow(
       <List
@@ -35,7 +31,8 @@ describe('<List/>', () => {
       />
     );
 
-    wrapper.find('Preview').at(3).dive().simulate('click');
+    const searchResult = wrapper.find(SearchResult).at(3); 
+    searchResult.props().onClick(searchResult.prop('result'));
     expect(onListItemClickedMock).toBeCalledWith(searchResults[3]);
   });
 });
