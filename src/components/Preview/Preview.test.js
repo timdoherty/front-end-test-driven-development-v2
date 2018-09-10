66eadf5e95db147db521e8fd35c3d3f98320cfb1
Tim Doherty <tim.doherty@procore.com>
2018-09-10 11:17:59 -0700

    Revert "Update Preview.test.js"
    This reverts commit 7831eddbbeddd67695fc1649c904ca1fa32e4704.

--- src/components/Preview/Preview.test.js
+++ src/components/Preview/Preview.test.js
@@ -3,17 +3,57 @@ import { shallow } from 'enzyme';
 
 import Preview from './Preview';
 import Thumbnail from '../Thumbnail';
-import searchResultsStubs from '../../stubs/searchResultsStub';
-import searchMetadataStubs from '../../stubs/searchMetadataStub';
-import searchSelector from '../../selector';
 
 describe('<Preview/>', () => {
-  const searchResult = searchSelector({
-    search: {
-      searchResults: searchResultsStubs, 
-      searchMetadata: searchMetadataStubs
-    }
-  }).searchResults[0];
+  const searchResult = {
+   "kind": "youtube#searchResult",
+   "etag": "\"XI7nbFXulYBIpL0ayR_gDh3eu1k/Va5WaCfLWg9_P0lB4Olj9aRA5ZI\"",
+   "id": {
+    "kind": "youtube#video",
+    "videoId": "2Gm7L3LEyz8"
+   },
+   "snippet": {
+    "publishedAt": "2017-09-09T02:09:57.000Z",
+    "channelId": "UCTg2TIWiAgr0FduTohrzj1A",
+    "title": "Pat Metheny Group - To The End of the World (1 Hour Extended)",
+    "description": "1 Hour extend of My favorite Metheny's song. Please check also my House Remix for this song. Pat Metheny - To the End of the World (Ymbk Borraginol Edit) ...",
+    "thumbnails": {
+     "default": {
+      "url": "https://i.ytimg.com/vi/2Gm7L3LEyz8/default.jpg",
+      "width": 120,
+      "height": 90
+     },
+     "medium": {
+      "url": "https://i.ytimg.com/vi/2Gm7L3LEyz8/mqdefault.jpg",
+      "width": 320,
+      "height": 180
+     },
+     "high": {
+      "url": "https://i.ytimg.com/vi/2Gm7L3LEyz8/hqdefault.jpg",
+      "width": 480,
+      "height": 360
+     }
+    },
+    "channelTitle": "slyellow2 Music",
+    "liveBroadcastContent": "none"
+   },
+   "id": "2Gm7L3LEyz8",
+   "contentDetails": {
+    "duration": "PT58M54S",
+    "dimension": "2d",
+    "definition": "hd",
+    "caption": "false",
+    "licensedContent": false,
+    "projection": "rectangular"
+   },
+   "statistics": {
+    "viewCount": "619624",
+    "likeCount": "4039",
+    "dislikeCount": "238",
+    "favoriteCount": "0",
+    "commentCount": "323"
+   }
+  };
 
   describe('thumbnail', () => {
     it('displays a default thumbnail', () => {
