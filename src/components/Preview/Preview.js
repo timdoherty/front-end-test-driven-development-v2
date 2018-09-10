d739cd7d92b469244683b9d3cf84c67aaa3f0deb
Tim Doherty <tim.doherty@procore.com>
2018-09-10 13:59:02 -0700

    Update Preview.js

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Thumbnail from '../Thumbnail/Thumbnail';
+import Thumbnail from '../Thumbnail';
 
 function Preview(props) {
   const {
