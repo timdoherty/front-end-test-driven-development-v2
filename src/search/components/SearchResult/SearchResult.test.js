d2209aa4389c114232d13a21d7aaeaed05f5a5c5
Tim Doherty <tim.doherty@procore.com>
2018-09-10 10:42:19 -0700

    remove dead comments

--- src/search/components/SearchResult/SearchResult.test.js
+++ src/search/components/SearchResult/SearchResult.test.js
@@ -15,56 +15,6 @@ describe('<SearchResult/>', () => {
     }
   }).searchResults[0];
 
-  // {
-  // "kind": "youtube#searchResult",
-  // "etag": "\"XI7nbFXulYBIpL0ayR_gDh3eu1k/Va5WaCfLWg9_P0lB4Olj9aRA5ZI\"",
-  // "id": {
-  //  "kind": "youtube#video",
-  //  "videoId": "2Gm7L3LEyz8"
-  // },
-  // "snippet": {
-  //  "publishedAt": "2017-09-09T02:09:57.000Z",
-  //  "channelId": "UCTg2TIWiAgr0FduTohrzj1A",
-  //  "title": "Pat Metheny Group - To The End of the World (1 Hour Extended)",
-  //  "description": "1 Hour extend of My favorite Metheny's song. Please check also my House Remix for this song. Pat Metheny - To the End of the World (Ymbk Borraginol Edit) ...",
-  //  "thumbnails": {
-  //   "default": {
-  //    "url": "https://i.ytimg.com/vi/2Gm7L3LEyz8/default.jpg",
-  //    "width": 120,
-  //    "height": 90
-  //   },
-  //   "medium": {
-  //    "url": "https://i.ytimg.com/vi/2Gm7L3LEyz8/mqdefault.jpg",
-  //    "width": 320,
-  //    "height": 180
-  //   },
-  //   "high": {
-  //    "url": "https://i.ytimg.com/vi/2Gm7L3LEyz8/hqdefault.jpg",
-  //    "width": 480,
-  //    "height": 360
-  //   }
-  //  },
-  //  "channelTitle": "slyellow2 Music",
-  //  "liveBroadcastContent": "none"
-  // },
-  // "id": "2Gm7L3LEyz8",
-  // "contentDetails": {
-  //  "duration": "PT58M54S",
-  //  "dimension": "2d",
-  //  "definition": "hd",
-  //  "caption": "false",
-  //  "licensedContent": false,
-  //  "projection": "rectangular"
-  // },
-  // "statistics": {
-  //  "viewCount": "619624",
-  //  "likeCount": "4039",
-  //  "dislikeCount": "238",
-  //  "favoriteCount": "0",
-  //  "commentCount": "323"
-  // }
-  //};
-
   it('displays a thumbnail', () => {
     const wrapper = shallow(
       <SearchResult result={searchResult} />
