178cb2eaf46cb273336018468439e8a0adc24667
Tim Doherty <tim.doherty@procore.com>
2018-09-14 10:29:54 -0700

    remove setSearchTerm action, update related container

--- src/search/components/SearchBar/Container.js
+++ src/search/components/SearchBar/Container.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connectModule } from 'redux-modules';
 
 import searchModule from '../../module';
 import SearchBar from './SearchBar';
 
 function SearchBarContainer(props) {
-  const { actions: { setSearchTerm }, ...rest } = props;
+  const { actions: { doSearch }, ...rest } = props;
   return (
     <SearchBar
-      onSearchChanged={setSearchTerm}
+      onSearchChanged={doSearch}
       {...rest}
     />
   );
 }
+
+SearchBarContainer.propTypes = {
+  actions: PropTypes.shape({
+    doSearch: PropTypes.func.isRequired
+  })
+};
 
 export default connectModule(searchModule)(SearchBarContainer);
