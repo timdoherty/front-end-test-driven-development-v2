fe72c4439937a36baf5074daf41791c61c34dbbd
Tim Doherty <tim.doherty@procore.com>
2018-09-14 14:41:24 -0700

    add proptypes to player and container

--- src/nowPlaying/components/Player/Container.js
+++ src/nowPlaying/components/Player/Container.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connectModule } from 'redux-modules';
 
 import nowPlayingModule from '../../module';
-import Player from './Player';
+import Player, { videoPropType } from './Player';
 
-export function PlayerContainer(props) {
+function PlayerContainer(props) {
   const { currentVideo } = props;
   return (
     <Player video={currentVideo} />
   );
 }
+
+PlayerContainer.propTypes = {
+  currentVideo: videoPropType
+};
 
 export default connectModule(nowPlayingModule)(PlayerContainer);
