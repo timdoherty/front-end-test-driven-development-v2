efdf64f349a9b78fc10c2d3a75b11d774b301169
Tim Doherty <tim.doherty@procore.com>
2018-09-14 14:24:12 -0700

    Revert "add proptypes to search list container"
    This reverts commit 5d15e789ae68954dae893e6fd285c2df27a751a6.

--- src/search/components/List/Container.js
+++ src/search/components/List/Container.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { connectModule } from 'redux-modules';
 
 import List from '../../../components/List';
 import searchModule from '../../module';
 import nowPlayingModule from '../../../nowPlaying/module';
 
-function SearchResultListContainer(props) {
+export function SearchResultListContainer(props) {
   const {
     search: {
       searchResults
@@ -25,16 +24,4 @@ function SearchResultListContainer(props) {
     />
   );
 }
-
-SearchResultListContainer.propTypes = {
-  search: PropTypes.shape({
-    searchResults: PropTypes.array.isRequired,
-  }),
-  actions: PropTypes.shape({
-    nowPlaying: PropTypes.shape({
-      setCurrentVideo: PropTypes.func.isRequired
-    })
-  }).isRequired
-};
-
 export default connectModule([searchModule, nowPlayingModule])(SearchResultListContainer);
