04afd4bb87de1ade2b2f7d979077ec7f485e7881
Tim Doherty <tim.doherty@procore.com>
2018-10-07 17:12:36 -0700

    write vanilla redux action creators and reducers

--- src/search/module.js
+++ src/search/module.js
@@ -1,24 +1,107 @@
-import { createModule } from 'redux-modules';
+let constants = {
+  GET_SEARCH_METADATA: 'GET_SEARCH_METADATA',
+  ON_SEARCH_FAILURE: 'ON_SEARCH_FAILURE',
+  ON_SEARCH_METADATA_SUCCESS: 'ON_SEARCH_METADATA_SUCCESS',
+  ON_SEARCH_METADATA_FAILURE: 'ON_SEARCH_METADATA_FAILURE',
+  ON_SEARCH_SUCCESS: 'ON_SEARCH_SUCCESS',
+  DO_SEARCH: 'DO_SEARCH'
+};
 
-import searchSelector from './selector';
-
-export default createModule({
-  name: 'search',
-  initialState: {},
-  selector: searchSelector,
-  transformations: {
-    setSearchTerm(state, action) {
-      const { payload } = action;
-      return {
-        ...state,
-        searchTerm: payload
-      };
-    },
-    clearSearchTerm(state, payload) {
-      return {
-        ...state,
-        searchTerm: null
-      };
-    }
+const actions = {
+  doSearch(payload, meta, error) {
+    return {
+      type: constants.DO_SEARCH,
+      payload,
+      meta,
+      error
+    };
+  },
+  getSearchMetadata(payload, meta, error) {
+    return {
+      type: constants.GET_SEARCH_METADATA,
+      payload,
+      meta,
+      error
+    };
+  },
+  onSearchFailure(payload, meta, error) {
+    return {
+      type: constants.ON_SEARCH_FAILURE,
+      payload,
+      meta,
+      error
+    };
+  },
+  onSearchMetadataSuccess(payload, meta, error) {
+    return {
+      type: constants.ON_SEARCH_METADATA_SUCCESS,
+      payload,
+      meta,
+      error
+    };
+  },
+  onSearchMetadataFailure(payload, meta, error) {
+    return {
+      type: constants.ON_SEARCH_METADATA_FAILURE,
+      payload,
+      meta,
+      error
+    };
+  },
+  onSearchSuccess(payload, meta, error) {
+    return {
+      type: constants.ON_SEARCH_SUCCESS,
+      payload,
+      meta,
+      error
+    };
   }
-});
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case constants.DO_SEARCH:
+      // TODO: do the search
+      return {
+        ...state,
+        isLoading: true,
+        searchTerm: action.payload
+      };
+    case constants.GET_SEARCH_METADATA:
+      // TODO: get the data
+      return {
+        ...state,
+        isLoading: true
+      };
+    case constants.ON_SEARCH_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      };
+    case constants.ON_SEARCH_METADATA_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        searchMetadata: action.payload.data
+      };
+    case constants.ON_SEARCH_METADATA_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      };
+    case constants.ON_SEARCH_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        searchResults: action.payload.data
+      };
+  }
+}
+
+export default {
+  actions,
+  constants,
+  reducer
+};
