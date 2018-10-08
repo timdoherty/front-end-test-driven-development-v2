669fca232acbcaef915f1e73c48b5f7ce11a3022
Tim Doherty <tim.doherty@procore.com>
2018-10-07 17:55:48 -0700

    refactor module to use a flux standard action utility method

--- src/search/module.js
+++ src/search/module.js
@@ -1,24 +1,77 @@
-import { createModule } from 'redux-modules';
+import { camelCase } from 'lodash';
 
-import searchSelector from './selector';
+function fluxStandardAction(type) {
+  return function fsa(payload, meta, error) {
+    return {
+      type,
+      payload,
+      meta,
+      error
+    };
+  };
+}
 
-export default createModule({
-  name: 'search',
-  initialState: {},
-  selector: searchSelector,
-  transformations: {
-    setSearchTerm(state, action) {
-      const { payload } = action;
+let constants = {
+  DO_SEARCH: 'DO_SEARCH',
+  GET_SEARCH_METADATA: 'GET_SEARCH_METADATA',
+  ON_SEARCH_FAILURE: 'ON_SEARCH_FAILURE',
+  ON_SEARCH_METADATA_SUCCESS: 'ON_SEARCH_METADATA_SUCCESS',
+  ON_SEARCH_METADATA_FAILURE: 'ON_SEARCH_METADATA_FAILURE',
+  ON_SEARCH_SUCCESS: 'ON_SEARCH_SUCCESS'
+};
+
+const actions = Object.keys(constants).reduce(
+  (accumulator, key) => {
+    accumulator[camelCase(key)] = fluxStandardAction(key);
+    return accumulator;
+  },
+  {}
+);
+
+function reducer(state, action) {
+  switch (action.type) {
+    case constants.DO_SEARCH:
+      // TODO: do the search
       return {
         ...state,
-        searchTerm: payload
+        isLoading: true,
+        searchTerm: action.payload
       };
-    },
-    clearSearchTerm(state, payload) {
+    case constants.GET_SEARCH_METADATA:
+      // TODO: get the data
       return {
         ...state,
-        searchTerm: null
+        isLoading: true
       };
-    }
+    case constants.ON_SEARCH_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      };
+    case constants.ON_SEARCH_METADATA_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        searchMetadata: action.payload.data
+      };
+    case constants.ON_SEARCH_METADATA_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      };
+    case constants.ON_SEARCH_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        searchResults: action.payload.data
+      };
   }
-});
+};
+
+export default {
+  actions,
+  constants,
+  reducer
+};
