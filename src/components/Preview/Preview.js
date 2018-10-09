258c7fa714f15929802737cb596fd7221bc33c5c
Tim Doherty <tim.doherty@procore.com>
2018-10-09 08:18:02 -0700

    Add general purpose video preview component; remove cruft from other steps

---
+++ src/components/Preview/Preview.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Card } from '@procore/core-react';
+import { Link } from 'react-router-dom';
+
+function Preview(props) {
+  const {
+    channelTitle,
+    description,
+    id,
+    thumbnail,
+    title,
+    viewCount
+  } = props;
+
+  return (
+    <Link to={`/nowPlaying/${id}`}>
+      <Card style={{
+        height: `${thumbnail.height}px`,
+        width: `${thumbnail.width}px`
+      }}>
+        <img
+          src={thumbnail.url}
+          height={thumbnail.height}
+          width={thumbnail.width}
+        />
+      </Card>
+      <span>{title}</span>
+      <span>{channelTitle}</span>
+      <span>{`${viewCount} views`}</span>
+      <span>{description}</span>
+    </Link>
+  );
+}
+
+Preview.propTypes = {
+  channelTitle: PropTypes.string,
+  description: PropTypes.string,
+  thumbnail: PropTypes.shape({
+    url: PropTypes.string,
+    width: PropTypes.number,
+    height: PropTypes.number
+  }),
+  title: PropTypes.string,
+  viewCount: PropTypes.string
+};
+
+Preview.defaultProps = {
+  channelTitle: '',
+  description: '',
+  thumbnail: {
+    url: '',
+    height: 0,
+    width: 0
+  },
+  title: '',
+  viewCount: ''
+};
+
+export default Preview;
