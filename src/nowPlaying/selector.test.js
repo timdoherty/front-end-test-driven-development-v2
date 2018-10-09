a362c9d1a718a3f948d3bc2db5630007493fc7cd
Tim Doherty <tim.doherty@procore.com>
2018-10-09 08:23:47 -0700

    pull in changes from prior steps

--- src/nowPlaying/selector.test.js
+++ src/nowPlaying/selector.test.js
@@ -2,56 +2,10 @@ import nowPlayingSelector from './selector';
 import commentStubs from './stubs/commentsStub';
 import relatedVideoStubs from './stubs/relatedVideosStub';
 import relatedVideoMetadataStubs from './stubs/relatedVideoMetadataStub';
-import { formatDuration } from '../utils';
+import { combineSearchData } from '../utils';
 
 describe('nowPlayingSelector', () => {
-  const currentVideo =   {
-   "kind": "youtube#searchResult",
-   "etag": "\"XI7nbFXulYBIpL0ayR_gDh3eu1k/Va5WaCfLWg9_P0lB4Olj9aRA5ZI\"",
-   "snippet": {
-    "publishedAt": "2017-09-09T02:09:57.000Z",
-    "channelId": "UCTg2TIWiAgr0FduTohrzj1A",
-    "title": "Pat Metheny Group - To The End of the World (1 Hour Extended)",
-    "description": "1 Hour extend of My favorite Metheny's song. Please check also my House Remix for this song. Pat Metheny - To the End of the World (Ymbk Borraginol Edit) ...",
-    "thumbnails": {
-     "default": {
-      "url": "https://i.ytimg.com/vi/2Gm7L3LEyz8/default.jpg",
-      "width": 120,
-      "height": 90
-     },
-     "medium": {
-      "url": "https://i.ytimg.com/vi/2Gm7L3LEyz8/mqdefault.jpg",
-      "width": 320,
-      "height": 180
-     },
-     "high": {
-      "url": "https://i.ytimg.com/vi/2Gm7L3LEyz8/hqdefault.jpg",
-      "width": 480,
-      "height": 360
-     }
-    },
-    "channelTitle": "slyellow2 Music",
-    "liveBroadcastContent": "none"
-   },
-   "kind": "youtube#video",
-   "etag": "\"XI7nbFXulYBIpL0ayR_gDh3eu1k/RRBaiYaP7zQ0RWQ4WuRnxO5qR7w\"",
-   "id": "2Gm7L3LEyz8",
-   "contentDetails": {
-    "duration": "PT58M54S",
-    "dimension": "2d",
-    "definition": "hd",
-    "caption": "false",
-    "licensedContent": false,
-    "projection": "rectangular"
-   },
-   "statistics": {
-    "viewCount": "619624",
-    "likeCount": "4039",
-    "dislikeCount": "238",
-    "favoriteCount": "0",
-    "commentCount": "323"
-   }
-  };
+  const currentVideo =   { foo: 'bar' };
 
   const state = {
     nowPlaying: {
@@ -80,19 +34,9 @@ describe('nowPlayingSelector', () => {
 
   describe('related videos', () => {
     it('gets videos related to the current video', () => {
-      const expected = relatedVideoStubs.items.map(result => {
-        const meta = relatedVideoMetadataStubs.items.find(
-          metadatum => metadatum.id === result.id.videoId
-        );
-        return {
-          ...result,
-          ...meta,
-          id: meta ? meta.id : result.id.videoId,
-          contentDetails: {
-            duration: meta ? formatDuration(meta.contentDetails.duration) : ''
-          }
-        };
-      });
+      const expected = combineSearchData(
+        relatedVideoStubs.items, relatedVideoMetadataStubs.items
+      );
 
       const actual = nowPlayingSelector(state).relatedVideos;
       expect(actual).toEqual(expected);
