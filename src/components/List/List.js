eddc25266f6fd13deae1c72556f9707d7263aff0
Tim Doherty <tim.doherty@procore.com>
2018-10-11 08:14:09 -0700

    implement comments list component; pull in changes from prior steps; add missing deps

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -31,25 +31,25 @@ const thumbnailProptype = PropTypes.shape({
   height: PropTypes.number
 });
 
-List.propTypes = {
-  listItems: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string,
-      snippet: PropTypes.shape({
-        title: PropTypes.string,
-        description: PropTypes.string,
-        channelTitle: PropTypes.string,
-        thumbnails: PropTypes.shape({
-          default: thumbnailProptype,
-          medium: thumbnailProptype,
-          high: thumbnailProptype
-        })
-      }),
-      statistics: PropTypes.shape({
-        viewCount: PropTypes.string
-      })
+export const previewPropType = PropTypes.shape({
+  id: PropTypes.string,
+  snippet: PropTypes.shape({
+    title: PropTypes.string,
+    description: PropTypes.string,
+    channelTitle: PropTypes.string,
+    thumbnails: PropTypes.shape({
+      default: thumbnailProptype,
+      medium: thumbnailProptype,
+      high: thumbnailProptype
     })
-  ),
+  }),
+  statistics: PropTypes.shape({
+    viewCount: PropTypes.string
+  })
+});
+
+List.propTypes = {
+  listItems: PropTypes.arrayOf(previewPropType),
   onListItemClicked: PropTypes.func,
   thumbnailSize: PropTypes.string,
   showDescription: PropTypes.bool
