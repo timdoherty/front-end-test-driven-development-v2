344d2f1ec26d4dd7e5c42fcbebe37d2b43f4eeb3
Tim Doherty <tim.doherty@procore.com>
2018-10-12 16:44:45 -0700

    pull in refactored code

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,22 +1,29 @@
 import moment from 'moment';
 
 export function formatDuration(duration) {
-  return moment.utc(moment.duration(duration).asMilliseconds()).format("HH:mm:ss");
+  return moment
+    .utc(moment.duration(duration).asMilliseconds())
+    .format('HH:mm:ss');
 }
 
 export function combineSearchData(searchResults, searchMeta) {
   return searchResults.map(result => {
-    const meta = searchMeta.find(
-      datum => datum.id === result.id.videoId
-    ) || { contentDetails: { duration: '' } };
+    const meta = searchMeta.find(datum => datum.id === result.id.videoId) || {
+      contentDetails: { duration: '' },
+      statistics: {},
+    };
     return {
-      ...result,
-      ...meta,
-      contentDetails: {
-        ...meta.contentDetails,
-        duration: formatDuration(meta.contentDetails.duration)
-      },
-      statistics: meta ? meta.statistics : {}
+      channelTitle: result.snippet.channelTitle,
+      commentCount: meta.statistics.commentCount,
+      description: result.snippet.description,
+      dislikeCount: meta.statistics.dislikeCount,
+      duration: formatDuration(meta.contentDetails.duration),
+      etag: result.etag,
+      id: result.id.videoId,
+      likeCount: meta.statistics.likeCount,
+      thumbnails: result.snippet.thumbnails,
+      title: result.snippet.title,
+      viewCount: meta.statistics.viewCount,
     };
   });
 }
