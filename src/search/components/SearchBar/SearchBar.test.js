dd060f8dc375c558445791b1b3ac0a2feff4c8fb
Tim Doherty <tim.doherty@procore.com>
2018-10-19 17:24:17 -0700

    add search term to search bar props
    (cherry picked from commit 88980fcf324da2912da08221bcc2e7dfd667fc3b)

--- src/search/components/SearchBar/SearchBar.test.js
+++ src/search/components/SearchBar/SearchBar.test.js
@@ -6,30 +6,38 @@ import SearchBar from './SearchBar';
 
 describe('<SearchBar/>', () => {
   it('has somewhere to enter search text', () => {
-    const wrapper = shallow(
-      <SearchBar />
-    );
+    const wrapper = shallow(<SearchBar />);
     expect(wrapper.find(Input).exists()).toBe.true;
+  });
+
+  it('displays a search term', () => {
+    const searchTerm = 'foobar';
+    const wrapper = shallow(<SearchBar searchTerm={searchTerm} />);
+    expect(wrapper.find(Input).prop('value')).toBe(searchTerm);
   });
 
   describe('when the user presses enter', () => {
     it('reacts when there is a search term', () => {
-      const onSearchChangedMock= jest.fn();
+      const onSearchChangedMock = jest.fn();
       const wrapper = shallow(
         <SearchBar onSearchChanged={onSearchChangedMock} />
       );
 
-      wrapper.find(Input).simulate('keyup', { key: 'Enter', target: { value: 'foo' } });
+      wrapper
+        .find(Input)
+        .simulate('keyup', { key: 'Enter', target: { value: 'foo' } });
       expect(onSearchChangedMock).toBeCalledWith('foo');
     });
 
     it('does not react when there is no search term', () => {
-      const onSearchChangedMock= jest.fn();
+      const onSearchChangedMock = jest.fn();
       const wrapper = shallow(
         <SearchBar onSearchChanged={onSearchChangedMock} />
       );
 
-      wrapper.find(Input).simulate('keyup', { key: 'Enter', target: { value: '' } });
+      wrapper
+        .find(Input)
+        .simulate('keyup', { key: 'Enter', target: { value: '' } });
       expect(onSearchChangedMock).not.toBeCalled();
     });
   });
