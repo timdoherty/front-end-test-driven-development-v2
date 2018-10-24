98b0e325bc3377b7c2a1fe6289a0e3d8daa2d02d
Tim Doherty <tim.doherty@procore.com>
2018-10-23 17:40:24 -0700

    modify search bar to be stateful, with controlled input
    (cherry picked from commit d2d38f954b963da8e4498d6d505f8055217b240c)

--- src/search/components/SearchBar/SearchBar.js
+++ src/search/components/SearchBar/SearchBar.js
@@ -1,29 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import { Input } from '@procore/core-react';
+import { Input, Button, Icon } from '@procore/core-react';
 
-function SearchBar(props) {
-  const { onSearchChanged, searchTerm } = props;
-  return (
-    <Input
-      value={searchTerm}
-      onChange={() => {}}
-      onKeyUp={({ key, target: { value } }) => {
-        if (key === 'Enter' && !!value) {
-          onSearchChanged(value);
-        }
-      }}
-    />
-  );
+export default class SearchBar extends Component {
+  static get propTypes() {
+    return {
+      onSearchChanged: PropTypes.func,
+    };
+  }
+
+  static get defaultProps() {
+    return {
+      onSearchChanged: Function.prototype,
+    };
+  }
+
+  constructor() {
+    super();
+    this.state = { searchTerm: '' };
+    this.onSearchTermChanged = this.onSearchTermChanged.bind(this);
+    this.onSearch = this.onSearch.bind(this);
+    this.onSearchTermKeyUp = this.onSearchTermKeyUp.bind(this);
+  }
+
+  componentDidMount() {
+    const { searchTerm } = this.props;
+    if (!!searchTerm) {
+      this.setState({ searchTerm });
+    }
+  }
+
+  onSearch() {
+    if (!!this.state.searchTerm) {
+      this.props.onSearchChanged(this.state.searchTerm);
+    }
+  }
+
+  onSearchTermChanged({ target: { value } }) {
+    this.setState({ searchTerm: value });
+  }
+
+  onSearchTermKeyUp({ key }) {
+    if (key === 'Enter') {
+      this.onSearch();
+    }
+  }
+
+  render() {
+    const { onSearchChanged, searchTerm } = this.props;
+    return (
+      <div>
+        <Input
+          value={this.state.searchTerm}
+          onChange={this.onSearchTermChanged}
+          onKeyUp={this.onSearchTermKeyUp}
+        />
+        <Button variant="secondary" onClick={this.onSearch}>
+          <Icon icon="search" />
+        </Button>
+      </div>
+    );
+  }
 }
-
-SearchBar.propTypes = {
-  onSearchChanged: PropTypes.func,
-};
-
-SearchBar.defaultProps = {
-  onSearchChanged: Function.prototype,
-};
-
-export default SearchBar;
--- src/search/components/SearchBar/SearchBar.test.js
+++ src/search/components/SearchBar/SearchBar.test.js
@@ -1,35 +1,65 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { Input } from '@procore/core-react';
+import { Input, Button } from '@procore/core-react';
 
 import SearchBar from './SearchBar';
 
 describe('<SearchBar/>', () => {
   it('has somewhere to enter search text', () => {
-    const wrapper = shallow(
-      <SearchBar />
-    );
+    const wrapper = shallow(<SearchBar />);
     expect(wrapper.find(Input).exists()).toBe.true;
+  });
+
+  it('initializes with the current search term', () => {
+    const searchTerm = 'foobar';
+    const wrapper = shallow(<SearchBar searchTerm={searchTerm} />);
+    expect(wrapper.find(Input).prop('value')).toBe(searchTerm);
   });
 
   describe('when the user presses enter', () => {
     it('reacts when there is a search term', () => {
-      const onSearchChangedMock= jest.fn();
+      const onSearchChangedMock = jest.fn();
       const wrapper = shallow(
         <SearchBar onSearchChanged={onSearchChangedMock} />
       );
 
-      wrapper.find(Input).simulate('keyup', { key: 'Enter', target: { value: 'foo' } });
+      wrapper.find(Input).simulate('change', { target: { value: 'foo' } });
+      wrapper.find(Input).simulate('keyup', { key: 'Enter' });
       expect(onSearchChangedMock).toBeCalledWith('foo');
     });
 
     it('does not react when there is no search term', () => {
-      const onSearchChangedMock= jest.fn();
+      const onSearchChangedMock = jest.fn();
       const wrapper = shallow(
         <SearchBar onSearchChanged={onSearchChangedMock} />
       );
 
-      wrapper.find(Input).simulate('keyup', { key: 'Enter', target: { value: '' } });
+      wrapper
+        .find(Input)
+        .simulate('keyup', { key: 'Enter', target: { value: '' } });
+      expect(onSearchChangedMock).not.toBeCalled();
+    });
+  });
+
+  describe('when the user clicks the search button', () => {
+    it('reacts when there is a search term', () => {
+      const onSearchChangedMock = jest.fn();
+      const wrapper = shallow(
+        <SearchBar onSearchChanged={onSearchChangedMock} />
+      );
+
+      wrapper.find(Input).simulate('change', { target: { value: 'foo' } });
+      wrapper.find(Button).simulate('click');
+      expect(onSearchChangedMock).toBeCalledWith('foo');
+    });
+
+    it('does not react when there is no search term', () => {
+      const onSearchChangedMock = jest.fn();
+      const wrapper = shallow(
+        <SearchBar onSearchChanged={onSearchChangedMock} />
+      );
+
+      wrapper.find(Button).simulate('click');
       expect(onSearchChangedMock).not.toBeCalled();
     });
   });
