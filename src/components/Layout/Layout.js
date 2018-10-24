56567775e203982ab6f02842b9d577ccd1208248
Tim Doherty <tim.doherty@procore.com>
2018-10-23 17:13:20 -0700

    update readme for step19; remove styling from layout component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -11,25 +11,26 @@ function Layout(props) {
   return (
     <div>
       <SearchBar />
-      <Route path='/search/:searchTerm' render={({ match }) => (
-        <SearchResultsListContainer key={match.params.searchTerm} />
-      )}/>
-      <Route path='/now-playing/:videoid' render={({ match }) => {
-        return (
-          <div style={{ display: 'flex' }}>
-            <div style={{ flex: 1 }}>
-              <Player key={match.params.videoid}/>
+      <Route
+        path="/search/:searchTerm"
+        render={({ match }) => (
+          <SearchResultsListContainer key={match.params.searchTerm} />
+        )}
+      />
+      <Route
+        path="/now-playing/:videoid"
+        render={({ match }) => {
+          return (
+            <div>
+              <Player key={match.params.videoid} />
               <Comments />
-            </div>
-            <div style={{ flex: '0 0 400px'}}>
               <RelatedVideos />
             </div>
-          </div>
-        )
-      }} />
+          );
+        }}
+      />
     </div>
   );
-} 
+}
 
 export default Layout;
-
