ec04011b6944c29c0cd56fa3daf131e8b84a7901
Tim Doherty <tim.doherty@procore.com>
2018-10-23 17:40:24 -0700

    modify search bar to be stateful, with controlled input
    (cherry picked from commit d2d38f954b963da8e4498d6d505f8055217b240c)

--- src/search/components/SearchBar/SearchBar.test.js
+++ src/search/components/SearchBar/SearchBar.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { Input } from '@procore/core-react';
+import { Input, Button } from '@procore/core-react';
 
 import SearchBar from './SearchBar';
 
@@ -10,7 +10,7 @@ describe('<SearchBar/>', () => {
     expect(wrapper.find(Input).exists()).toBe.true;
   });
 
-  it('displays a search term', () => {
+  it('initializes with the current search term', () => {
     const searchTerm = 'foobar';
     const wrapper = shallow(<SearchBar searchTerm={searchTerm} />);
     expect(wrapper.find(Input).prop('value')).toBe(searchTerm);
@@ -23,9 +23,8 @@ describe('<SearchBar/>', () => {
         <SearchBar onSearchChanged={onSearchChangedMock} />
       );
 
-      wrapper
-        .find(Input)
-        .simulate('keyup', { key: 'Enter', target: { value: 'foo' } });
+      wrapper.find(Input).simulate('change', { target: { value: 'foo' } });
+      wrapper.find(Input).simulate('keyup', { key: 'Enter' });
       expect(onSearchChangedMock).toBeCalledWith('foo');
     });
 
@@ -38,6 +37,29 @@ describe('<SearchBar/>', () => {
       wrapper
         .find(Input)
         .simulate('keyup', { key: 'Enter', target: { value: '' } });
+      expect(onSearchChangedMock).not.toBeCalled();
+    });
+  });
+
+  describe('when the user clicks the search button', () => {
+    it('reacts when there is a search term', () => {
+      const onSearchChangedMock = jest.fn();
+      const wrapper = shallow(
+        <SearchBar onSearchChanged={onSearchChangedMock} />
+      );
+
+      wrapper.find(Input).simulate('change', { target: { value: 'foo' } });
+      wrapper.find(Button).simulate('click');
+      expect(onSearchChangedMock).toBeCalledWith('foo');
+    });
+
+    it('does not react when there is no search term', () => {
+      const onSearchChangedMock = jest.fn();
+      const wrapper = shallow(
+        <SearchBar onSearchChanged={onSearchChangedMock} />
+      );
+
+      wrapper.find(Button).simulate('click');
       expect(onSearchChangedMock).not.toBeCalled();
     });
   });
