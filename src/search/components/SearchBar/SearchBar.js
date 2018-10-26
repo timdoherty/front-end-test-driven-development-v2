93371d341be222fbc2e3e3121d7c01b925cb7400
Tim Doherty <tim.doherty@procore.com>
2018-10-26 14:13:27 -0700

    pull in commits from prior step

--- src/search/components/SearchBar/SearchBar.js
+++ src/search/components/SearchBar/SearchBar.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connectModule } from 'redux-modules';
 import { withRouter } from 'react-router';
+import { compose } from 'ramda';
+import { Input, Button } from '@procore/core-react';
 
-import { Input, Button, Icon } from '@procore/core-react';
+import searchModule from '../../module';
 
-class SearchBar extends Component {
+const { actions, selector } = searchModule;
+
+export class SearchBar extends Component {
   static get propTypes() {
     return {
       actions: PropTypes.shape({
         doSearch: PropTypes.func,
+      }),
+      history: PropTypes.shape({
+        push: PropTypes.func,
       }),
       searchTerm: PropTypes.string,
     };
@@ -17,6 +25,7 @@ class SearchBar extends Component {
   static get defaultProps() {
     return {
       actions: { doSearch() {} },
+      history: { push() {} },
       searchTerm: '',
     };
   }
@@ -33,7 +42,7 @@ class SearchBar extends Component {
 
   doSearch() {
     if (!!this.state.searchTerm) {
-      this.props.onSearchChanged(this.state.searchTerm);
+      this.props.actions.doSearch(this.state.searchTerm);
       this.props.history.push(`/search/${this.state.searchTerm}`);
     }
   }
@@ -62,4 +71,7 @@ class SearchBar extends Component {
   }
 }
 
-export const SearchBarWithRouter = withRouter(SearchBar);
+export default compose(
+  connectModule(searchModule),
+  withRouter
+)(SearchBar);
