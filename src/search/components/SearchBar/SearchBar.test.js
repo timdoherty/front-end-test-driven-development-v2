019d60e765cd10bb7bfb25c667602cd859804626
Tim Doherty <tim.doherty@procore.com>
2018-10-26 09:49:47 -0700

    refactor search bar container to use withRouter directly
    (cherry picked from commit e3d8ab0b1b16d0d5ea3c963601035905b8bd99b7)

--- src/search/components/SearchBar/SearchBar.test.js
+++ src/search/components/SearchBar/SearchBar.test.js
@@ -1,68 +1,80 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import { Input, Button } from '@procore/core-react';
+import { mount } from 'enzyme';
+import { MemoryRouter } from 'react-router';
 
-import SearchBar from './SearchBar';
+import { SearchBarWithRouter as SearchBar } from './SearchBar';
 
 describe('<SearchBar/>', () => {
+  let wrapper;
+
+  function render(props) {
+    return mount(
+      <MemoryRouter>
+        <SearchBar {...props} />
+      </MemoryRouter>
+    );
+  }
+
   it('has somewhere to enter search text', () => {
-    const wrapper = shallow(<SearchBar />);
-    expect(wrapper.find(Input).exists()).toBe.true;
+    wrapper = render({});
+    expect(wrapper.find('Input').exists()).toBe(true);
   });
 
   it('initializes with the current search term', () => {
     const searchTerm = 'foobar';
-    const wrapper = shallow(<SearchBar searchTerm={searchTerm} />);
-    expect(wrapper.find(Input).prop('value')).toBe(searchTerm);
+    wrapper = render({ searchTerm });
+    expect(wrapper.find('Input').prop('value')).toBe(searchTerm);
   });
 
   describe('when the user presses enter', () => {
-    it('reacts when there is a search term', () => {
+    it('responds when there is a search term', () => {
       const onSearchChangedMock = jest.fn();
-      const wrapper = shallow(
-        <SearchBar onSearchChanged={onSearchChangedMock} />
-      );
-
-      wrapper.find(Input).simulate('change', { target: { value: 'foo' } });
-      wrapper.find(Input).simulate('keyup', { key: 'Enter' });
-      expect(onSearchChangedMock).toBeCalledWith('foo');
-    });
-
-    it('does not react when there is no search term', () => {
-      const onSearchChangedMock = jest.fn();
-      const wrapper = shallow(
-        <SearchBar onSearchChanged={onSearchChangedMock} />
-      );
+      const searchTerm = 'foo';
+      wrapper = render({ onSearchChanged: onSearchChangedMock });
 
       wrapper
-        .find(Input)
+        .find('Input')
+        .simulate('change', { target: { value: searchTerm } });
+      wrapper.find('Input').simulate('keyup', { key: 'Enter' });
+
+      expect(onSearchChangedMock).toBeCalledWith(searchTerm);
+      expect(wrapper.find('Router').prop('history').location.pathname).toBe(
+        `/search/${searchTerm}`
+      );
+    });
+
+    it('does not respond when there is no search term', () => {
+      const onSearchChangedMock = jest.fn();
+      wrapper = render({ onSearchChanged: onSearchChangedMock });
+
+      wrapper
+        .find('Input')
         .simulate('keyup', { key: 'Enter', target: { value: '' } });
+
       expect(onSearchChangedMock).not.toBeCalled();
     });
   });
 
   describe('when the user clicks the search button', () => {
-    it('reacts when there is a search term', () => {
+    it('responds when there is a search term', () => {
       const onSearchChangedMock = jest.fn();
       const searchTerm = 'fluffy unicorns';
-      const wrapper = shallow(
-        <SearchBar
-          onSearchChanged={onSearchChangedMock}
-          searchTerm={searchTerm}
-        />
-      );
+      wrapper = render({ onSearchChanged: onSearchChangedMock, searchTerm });
 
-      wrapper.find(Button).simulate('click');
+      wrapper.find('Button').simulate('click');
+
       expect(onSearchChangedMock).toBeCalledWith(searchTerm);
+      expect(wrapper.find('Router').prop('history').location.pathname).toBe(
+        `/search/${searchTerm}`
+      );
     });
 
-    it('does not react when there is no search term', () => {
+    it('does not respond when there is no search term', () => {
       const onSearchChangedMock = jest.fn();
-      const wrapper = shallow(
-        <SearchBar onSearchChanged={onSearchChangedMock} />
-      );
+      wrapper = render({ onSearchChanged: onSearchChangedMock });
 
-      wrapper.find(Button).simulate('click');
+      wrapper.find('Button').simulate('click');
+
       expect(onSearchChangedMock).not.toBeCalled();
     });
   });
